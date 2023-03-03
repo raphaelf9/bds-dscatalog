fb2a1d7f4e6cf67235ed6649e0a53041bf704701
Raphael Faria <raphaelf9@outlook.com>
2023-03-02 21:06:53 -0300

    Project clean

--- frontweb/src/index.tsx
+++ frontweb/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDom.render(
+ReactDOM.render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>,
